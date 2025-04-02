11bc861fd714ddd09729b0c2ee41919b5553a71c
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2025-04-02 18:07:49 -0300

    Incorporacion de imagenes webp y creación de sealed class HoroscopeInfo

--- app/src/main/java/com/javimutis/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/javimutis/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.javimutis.horoscapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
+    //conexión entre viewmodel y fragment
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
 
