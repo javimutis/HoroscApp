e9be84fb8821e8123cab9284b17b7dbe0f7109a6
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2025-04-15 11:53:01 -0400

    Basic Testing

--- app/src/test/java/com/javimutis/horoscapp/ui/horoscope/HoroscopeViewModelTest.kt
+++ app/src/test/java/com/javimutis/horoscapp/ui/horoscope/HoroscopeViewModelTest.kt
@@ -1,28 +1,34 @@
 package com.javimutis.horoscapp.ui.horoscope
 
 import com.javimutis.horoscapp.data.providers.HoroscopeProvider
+import com.javimutis.horoscapp.motherObject.HoroscopeMotherObject.horoscopeInfoList
 import io.mockk.MockKAnnotations
+import io.mockk.MockKAnnotations.init
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
-class HoroscopeViewModelTest {
+class HoroscopeViewModelTest{
 
     @MockK
     lateinit var horoscopeProvider: HoroscopeProvider
 
-    private lateinit var viewModel: HoroscopeViewModel
+    private lateinit var viewModel:HoroscopeViewModel
 
     @Before
-    fun setUp() {
-        MockKAnnotations.init(this, relaxUnitFun = true)
+    fun setUp(){
+        init(this, relaxUnitFun = true)
     }
 
     @Test
-    fun `when viewmodel is created then getHoroscope are loaded`() {
-        every{ horoscopeProvider.getHoroscopes()} returns listOf()
+    fun `when viewmodel is created then horoscopes are loaded`(){
+        every { horoscopeProvider.getHoroscopes() } returns horoscopeInfoList
         viewModel = HoroscopeViewModel(horoscopeProvider)
-    }
 
+        val horoscopes = viewModel.horoscope.value
+
+        assertTrue(horoscopes.isNotEmpty())
+    }
 }
